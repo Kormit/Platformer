ca3850ae0f4fe07524b238b7b422b2b1518cfd05
Kormit <43767844+Kormit@users.noreply.github.com>
2018-10-03 16:57:40 +1000

    Added collision code
    Added collision checks, which currently work to stop the character walking through walls to the left of him. To be completed

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -5,13 +5,16 @@ using MonoGame.Extended;
 using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Graphics;
 using MonoGame.Extended.ViewportAdapters;
+using System.Collections;
 
 
 namespace Game1
 {
     
+
     public class Game1 : Game
     {
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
@@ -21,22 +24,35 @@ namespace Game1
         TiledMap map = null; // Creates an instance of a Tiled map
         TiledMapRenderer mapRenderer = null; // Creates an instance of what makes a tiled map
 
+        TiledMapTileLayer collisionLayer;
+        public ArrayList allCollisionTiles = new ArrayList();
+        public Sprite[,] levelGrid;
+
+        public int tileHeight = 0;
+        public int levelTileWidth = 0;
+        public int levelTileHeight = 0;
+
+        public Rectangle myMap = new Rectangle();
+
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            
+            graphics.PreferredBackBufferWidth = 1600;
+            graphics.PreferredBackBufferHeight = 900;
+
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            myMap.X = 0;
+            myMap.Y = 0;
+            myMap.Width = 6400; // each block is 64 by 64 px
+            myMap.Height = 6400;
 
             base.Initialize();
         }
@@ -48,7 +64,8 @@ namespace Game1
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
 
-            player.Load(Content, this); // Call the 'Load' Function in the player class
+            player.Load(Content, this); // Call the 'Load' Function in the player class. 
+            // 'this' basically means "pass all information in our class through as a variable"
 
             BoxingViewportAdapter viewportAdapter = new BoxingViewportAdapter(Window,
                                                                                GraphicsDevice,
@@ -61,22 +78,62 @@ namespace Game1
             map = Content.Load<TiledMap>("Level 1");
             mapRenderer = new TiledMapRenderer(GraphicsDevice);
 
+            SetUpTiles();
+
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+       
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+            
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public void SetUpTiles()
+        {
+            tileHeight = map.TileHeight;
+            levelTileHeight = map.Height;
+            levelTileWidth = map.Width;
+            levelGrid = new Sprite[levelTileWidth, levelTileHeight];
+            foreach(TiledMapTileLayer layer in map.TileLayers)
+            {
+                if(layer.Name == "Collision")
+                {
+                    collisionLayer = layer;
+                }
+            }
+
+            int columns = 0;
+            int rows = 0;
+            int loopCount = 0;
+            while (loopCount < collisionLayer.Tiles.Count)
+            {
+                if(collisionLayer.Tiles[loopCount].GlobalIdentifier != 0)
+                {
+                    Sprite tileSprite = new Sprite();
+                    tileSprite.position.X = columns * tileHeight;
+                    tileSprite.position.Y = rows * tileHeight;
+                    tileSprite.width = tileHeight;
+                    tileSprite.height = tileHeight;
+                    tileSprite.UpdateHitBox();
+                    allCollisionTiles.Add(tileSprite);
+                    levelGrid[columns, rows] = tileSprite;
+                }
+
+                columns++;
+
+                if(columns == levelTileWidth)
+                {
+                    columns = 0;
+                    rows++;
+                }
+
+                loopCount++;
+                
+            }
+
+        }
+
+        
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -84,6 +141,10 @@ namespace Game1
 
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             player.Update(deltaTime); // Call the 'Update' from our player class
+
+            //This makes the camera follow the player's position, it also should center the player to the centre of the camera
+            camera.Position = player.playerSprite.position - new Vector2(graphics.GraphicsDevice.Viewport.Width / 2,
+                                                                            graphics.GraphicsDevice.Viewport.Height / 2);
 
             base.Update(gameTime);
         }
@@ -111,8 +172,8 @@ namespace Game1
             mapRenderer.Draw(map, ref viewMatrix, ref projectionMatrix);
             //call the 'Draw' function from our player class
 
-            camera.Position = player.playerSprite.position - new Vector2(graphics.GraphicsDevice.Viewport.Width / 2,
-                                                                            graphics.GraphicsDevice.Viewport.Height / 2);
+            //camera.Position = player.playerSprite.position - new Vector2(graphics.GraphicsDevice.Viewport.Width / 2,
+                                                                            //graphics.GraphicsDevice.Viewport.Height / 2);
 
             player.Draw(spriteBatch);
             //finish drawing
--- Game1/Game1/Sprite.cs
+++ Game1/Game1/Sprite.cs
@@ -4,36 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Game1
 {
-    class Sprite
+    public class Sprite
     {
         public Vector2 position = Vector2.Zero;
+        public Vector2 velocity = Vector2.Zero;
         public Vector2 offset = Vector2.Zero;
 
+
         Texture2D texture;
+
+        public int width = 0;
+        public int height = 0;
+
+        //The edges of the sprite are also the edges of the hitbox for collisions 
+
+        public int leftEdge = 0;
+        public int rightEdge = 0;
+        public int topEdge = 0;
+        public int bottomEdge = 0;
 
         public Sprite()
         {
 
         }
 
-        public void Load(ContentManager content, string asset)
+        public void Load(ContentManager content, string asset, bool useOffset)
         {
             texture = content.Load<Texture2D>(asset);
+            width = texture.Bounds.Width;
+            height = texture.Bounds.Height;
+
+            if(useOffset == true)
+            {
+                offset = new Vector2(leftEdge + width / 2, topEdge + height / 2);
+            }
+
+            UpdateHitBox();
         }
+
+
         
+        public void UpdateHitBox()
+        {
+            leftEdge = (int)position.X - (int)offset.X;
+            rightEdge = leftEdge + width;
+            topEdge = (int)position.Y - (int)offset.Y;
+            bottomEdge = topEdge + height;
+        }
+
         public void Update(float deltaTime)
         {
-
+            
         }
         
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, position + offset, Color.White);
+            spriteBatch.Draw(texture, position - offset, Color.White);
         }
 
        
